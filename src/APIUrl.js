beb357b5c140100f9e8d2a6b114e37d75cfe3177
WayneStB <103074933+WayneStB@users.noreply.github.com>
2022-08-17 17:58:35 -0400

    Update APIUrl.js

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,8 @@
-const APIUrl = "https://waynestb-blog-backend.herokuapp.com";
+let APIUrl = "http://localhost:3001";
+
+// handles if we are running heroku
+if (window.location.host.indexOf(".herokuapp.com") !== -1) {
+    APIUrl = "https://waynestb-blog-backend.herokuapp.com";
+}
 
 export default APIUrl;
